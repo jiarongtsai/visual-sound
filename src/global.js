03eb93a4da7f20be1f357622a4e834ef87459b98
jiarongtsai <jiarongtsai19@gmail.com>
2022-04-11 13:41:31 +0800

    [style] add globalstyles and adjust navbar

---
+++ src/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Mukta', sans-serif;
+    text-rendering: optimizeLegibility;
+  }
+  `;
