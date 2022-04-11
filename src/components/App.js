ee94fa54af0dd211b1edc107b83702d1c2aff62a
jiarongtsai <jiarongtsai19@gmail.com>
2022-04-11 13:41:31 +0800

    [style] add globalstyles and adjust navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { GlobalStyles } from "../global";
+import styled from "styled-components";
 import Main from "../pages/Main";
 import Profile from "../pages/Profile";
 import Community from "../pages/Community";
 import Explore from "../pages/Explore";
 
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  width: 50vw;
+`;
+
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <p>Visual Sound - personal project</p>
-      <nav>
+      <Nav>
         <Link to="/">Main</Link>
         <Link to="/community">Community</Link>
         <Link to="/explore">Explore</Link>
         <Link to="/profile">Profile</Link>
-      </nav>
+      </Nav>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="profile" element={<Profile />} />
