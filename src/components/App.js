1ad9d0a20a7d787a10dd7005797cc92eaa7a8016
jiarongtsai <jiarongtsai19@gmail.com>
2022-04-13 16:55:44 +0800

    [refactor] remove unnecessary file

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import Main from "../pages/Main";
 import Profile from "../pages/Profile";
 import Community from "../pages/Community";
 import Explore from "../pages/Explore";
-import TonePlayer from "../pages/TonePlayer";
 
 const Nav = styled.nav`
   display: flex;
@@ -21,14 +20,12 @@ function App() {
       <p>Visual Sound - personal project</p>
       <Nav>
         <Link to="/">Main</Link>
-        <Link to="/tonePlayer">TonePlayer</Link>
         <Link to="/community">Community</Link>
         <Link to="/explore">Explore</Link>
         <Link to="/profile">Profile</Link>
       </Nav>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="tonePlayer" element={<TonePlayer />} />
         <Route path="profile" element={<Profile />} />
         <Route path="community" element={<Community />} />
         <Route path="explore" element={<Explore />} />
