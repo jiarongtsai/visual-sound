192d49be1a00ef39b89eb48e6506e2fbc0f14ab1
jiarongtsai <jiarongtsai19@gmail.com>
2022-04-15 14:25:23 +0800

    [feat] limited and ordered data of explore page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,10 @@
 import { React } from "react";
 import MySequencer from "../components/squencer/MySequencer";
-import Header from "../components/Header";
 import { PlayerProvider } from "../components/PlayerProvider";
 
 export default function Main() {
   return (
     <>
-      <Header />
       <PlayerProvider>
         {({ soundPlayer }) => {
           return <MySequencer player={soundPlayer} />;
