348afa6f5bfee7058a598dd50e3e056b6fb7c05f
jiarongtsai <jiarongtsai19@gmail.com>
2022-04-16 16:14:29 +0800

    [feat] add search fucntion

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -5,15 +5,23 @@ import SequencePlayer from "../components/SequencePlayer";
 
 export default function Explore() {
   const [exploreworks, setExploreworks] = useState([]);
+  const [input, setInput] = useState("");
   useEffect(() => {
     Firebase.getAllworks().then((data) => setExploreworks(data));
   }, []);
 
+  function handleSearch() {
+    Firebase.searchWorks(input).then((data) => {
+      setExploreworks(data);
+      setInput("");
+    });
+  }
+
   return (
     <>
       <div>Explore</div>
-      <input />
-      <button>Search</button>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleSearch}>Search</button>
       {exploreworks.map((work, i) => {
         return (
           <div key={i}>
