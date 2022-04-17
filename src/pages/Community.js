b9ee830ed735927876f31ea4ee25d7951a2b8ef0
jiarongtsai <jiarongtsai19@gmail.com>
2022-04-17 14:05:05 +0800

    [feat] add like and collect feature to community page

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -21,11 +21,59 @@ const Div = styled.div`
 export default function Community() {
   const [allworks, setAllworks] = useState([]);
   const [workModalID, setWorkModalID] = useState("");
+  const [like, setLike] = useState([]);
+  const [collect, setCollect] = useState([]);
+
   useEffect(() => {
     Firebase.getFollowingWorks(userID).then((data) => {
       setAllworks(data);
+      data.forEach((item) => {
+        if (item.liked_by.includes(userID)) {
+          setLike((pre) => [...pre, true]);
+        } else {
+          setLike((pre) => [...pre, false]);
+        }
+        if (item.collected_by.includes(userID)) {
+          setCollect((pre) => [...pre, true]);
+        } else {
+          setCollect((pre) => [...pre, false]);
+        }
+      });
     });
   }, []);
+
+  function handleLike(id, i, list) {
+    if (!like[i]) {
+      Firebase.likeWork(userID, id, list).then(() => {
+        const newLikeList = [...like];
+        newLikeList[i] = !newLikeList[i];
+        setLike(newLikeList);
+      });
+      return;
+    }
+
+    Firebase.unlikeWork(userID, id, list).then(() => {
+      const newLikeList = [...like];
+      newLikeList[i] = !newLikeList[i];
+      setLike(newLikeList);
+    });
+  }
+
+  function handleCollect(id, i, list) {
+    if (!collect[i]) {
+      Firebase.collectWork(userID, id, list).then(() => {
+        const newCollectList = [...collect];
+        newCollectList[i] = !newCollectList[i];
+        setCollect(newCollectList);
+        return;
+      });
+    }
+    Firebase.uncollectWork(userID, id, list).then(() => {
+      const newCollectList = [...collect];
+      newCollectList[i] = !newCollectList[i];
+      setCollect(newCollectList);
+    });
+  }
   return (
     <>
       {workModalID ? (
@@ -33,7 +81,7 @@ export default function Community() {
       ) : (
         ""
       )}
-      {allworks.map((work) => {
+      {allworks.map((work, i) => {
         return (
           <div
             key={work.id}
@@ -55,9 +103,17 @@ export default function Community() {
               }}
             </PlayerProvider>
             <div>
-              <span>❤️</span>
-              <span>💬</span>
-              <span>⭐️</span>
+              <button onClick={() => handleLike(work.id, i, work.liked_by)}>
+                {`${like[i] ? "liked" : "like"}`}
+              </button>
+              <button
+                onClick={() => handleCollect(work.id, i, work.collected_by)}
+              >
+                {`${collect[i] ? "collected" : "collect"}`}
+              </button>
+              <button onClick={() => setWorkModalID(work.id)}>
+                Add a comment
+              </button>
             </div>
             <Div>
               <h3>{work.author_name}</h3>
@@ -80,6 +136,7 @@ export default function Community() {
                 style={{ cursor: "pointer" }}
                 onClick={() => setWorkModalID(work.id)}
               >{`view all ${work.comments_count} comments`}</a>
+              <br />
             </div>
             <p>{work.created_time.toDate().toDateString()}</p>
           </div>
