1e49feb5b04fdd0fb3e6cf1db2e5f596e9f4129c
jiarongtsai <jiarongtsai19@gmail.com>
2022-06-08 19:17:23 +0800

    [refactor] code spliting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, Suspense, lazy } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 import RequireAuth from "./components/auth/RequireAuth";
+import Navbar from "./components/Navbar";
 import Main from "./pages/Main";
-import Profile from "./pages/profile/Profile";
-import ProfileWorks from "./pages/profile/ProfileWorks";
-import ProfileCollections from "./pages/profile/ProfileCollections";
-import Message from "./pages/message/Message";
-import Community from "./pages/community/Community";
 import Explore from "./pages/Explore";
-import User from "./pages/User";
-import WorkModal from "./pages/work/WorkModal";
-import WorkView from "./pages/work/WorkView";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/Navbar";
+import Loader from "./components/Loader";
+const Profile = lazy(() => import("./pages/profile/Profile"));
+const ProfileWorks = lazy(() => import("./pages/profile/ProfileWorks"));
+const ProfileCollections = lazy(() =>
+  import("./pages/profile/ProfileCollections")
+);
+const Message = lazy(() => import("./pages/message/Message"));
+const Community = lazy(() => import("./pages/community/Community"));
+const WorkModal = lazy(() => import("./pages/work/WorkModal"));
+const WorkView = lazy(() => import("./pages/work/WorkView"));
+const User = lazy(() => import("./pages/User"));
 
 function App() {
   const location = useLocation();
@@ -23,75 +26,79 @@ function App() {
 
   return (
     <>
-      <Routes location={state?.backgroundLocation || location}>
-        <Route path="/" element={<Navbar />}>
-          <Route index element={<Main />} />
-          <Route
-            path="community"
-            element={
-              <RequireAuth>
-                <Community
+      <Suspense fallback={<Loader />}>
+        <Routes location={state?.backgroundLocation || location}>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Main />} />
+            <Route path="explore" element={<Explore />} />
+            <Route path="login" element={<Login />} />
+
+            <Route
+              path="community"
+              element={
+                <RequireAuth>
+                  <Community
+                    followingWorks={followingWorks}
+                    setFollowingWorks={setFollowingWorks}
+                  />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="profile"
+              element={
+                <RequireAuth>
+                  <Profile />
+                </RequireAuth>
+              }
+            >
+              <Route index element={<ProfileWorks />} />
+              <Route path="collection" element={<ProfileCollections />} />
+            </Route>
+            <Route
+              path="message"
+              element={
+                <RequireAuth>
+                  <Message />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="message/:mid"
+              element={
+                <RequireAuth>
+                  <Message />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/work/:id"
+              element={
+                <WorkView
                   followingWorks={followingWorks}
                   setFollowingWorks={setFollowingWorks}
                 />
-              </RequireAuth>
-            }
-          />
-          <Route path="explore" element={<Explore />} />
-          <Route
-            path="/work/:id"
-            element={
-              <WorkView
-                followingWorks={followingWorks}
-                setFollowingWorks={setFollowingWorks}
-              />
-            }
-          />
-
-          <Route path="/user/:uid" element={<User />} />
-          <Route
-            path="profile"
-            element={
-              <RequireAuth>
-                <Profile />
-              </RequireAuth>
-            }
-          >
-            <Route index element={<ProfileWorks />} />
-            <Route path="collection" element={<ProfileCollections />} />
+              }
+            />
+            <Route path="/user/:uid" element={<User />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
-          <Route
-            path="message"
-            element={
-              <RequireAuth>
-                <Message />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="message/:mid"
-            element={
-              <RequireAuth>
-                <Message />
-              </RequireAuth>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-      {state?.backgroundLocation && (
-        <Routes>
-          <Route
-            path="/work/:id"
-            element={
-              <WorkModal
-                followingWorks={followingWorks}
-                setFollowingWorks={setFollowingWorks}
-              />
-            }
-          />
         </Routes>
+      </Suspense>
+      {state?.backgroundLocation && (
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route
+              path="/work/:id"
+              element={
+                <WorkModal
+                  followingWorks={followingWorks}
+                  setFollowingWorks={setFollowingWorks}
+                />
+              }
+            />
+          </Routes>
+        </Suspense>
       )}
     </>
   );
